1706ab0460f1654ef2a1aef49339c3c1e8181d17
Owaski <owaski123@gmail.com>
2021-04-19 19:31:47 +0800

    organize order distributions in flow.utils

---
+++ flow/utils/distributions.py
@@ -0,0 +1,170 @@
+import numpy as np
+
+def gen_request(env):
+    tp = 0
+    if env.distribution == 'random':
+        idx = env.k.person.total
+        edge_list = env.edges.copy()
+        edge_id1 = np.random.choice(edge_list)
+        edge_list.remove(edge_id1)
+        edge_id2 = np.random.choice(edge_list)
+
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-1': 
+        # the request only appears at one edge
+        idx = env.k.person.total
+        edge_list = env.edges.copy()
+        edge_id1 = 'bot3_1_0'
+        edge_list.remove(edge_id1)
+        edge_id2 = 'top2_2_0'
+
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-11': 
+        # the request only appears at one edge
+        idx = env.k.person.total
+        edge_list = env.edges.copy()
+        edge_id1 = 'bot3_1_0'
+        edge_list.remove(edge_id1)
+        edge_id2 = 'top1_2_0'
+
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-12': 
+        # the request only appears at one edge
+        idx = env.k.person.total
+        edge_list = env.edges.copy()
+        edge_id1 = 'bot3_1_0'
+        edge_list.remove(edge_id1)
+        edge_id2 = 'top2_3_0'
+
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-13': 
+        idx = env.k.person.total
+        rn =  np.random.rand()
+        edge_id1 = 'bot3_1_0'
+        edge_id2 = 'left1_3_0' if rn < 0.5 else 'bot0_3_0'
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-14':
+        idx = env.k.person.total
+        rn =  np.random.rand()
+        edge_id1 = np.random.choice(['bot3_1_0', 'top3_1_0', 'left3_0_0', 'right3_0_0', \
+            'left3_1_0', 'right3_1_0', 'top2_1_0', 'bot2_1_0'])
+        edge_id2 = 'left1_3_0' if rn < 0.5 else 'bot0_3_0'
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-15':
+        idx = env.k.person.total
+        edge_id1 = 'bot3_1_0'
+        edge_id2 = 'top0_2_0'
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-X':
+        # the request only appears at one edge
+        idx = env.k.person.total
+        rn =  np.random.rand()
+        edge_id1 = 'bot3_1_0' if rn < 0.5 else 'top3_3_0'
+        edge_id2 = 'top0_3_0' if rn < 0.5 else 'bot0_1_0'
+        tp = 0 if rn < 0.5 else 1
+
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-X1':
+        idx = env.k.person.total
+        rn, rn2 =  np.random.rand(), np.random.rand()
+        if rn < 0.5:
+            edge_id1 = 'bot3_1_0'
+            edge_id2 = 'left1_3_0' if rn2 < 0.5 else 'bot0_3_0'
+            tp = 0
+        else:
+            edge_id1 = 'top3_3_0'
+            edge_id2 = 'left1_0_0' if rn2 < 0.5 else 'top0_1_0'
+            tp = 1
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-X1-1':
+        idx = env.k.person.total
+        rn, rn2 =  np.random.rand(), np.random.rand()
+        if rn < 0.5:
+            edge_id1 = 'bot3_1_0'
+            edge_id2 = 'left1_3_0' if rn2 < 0.5 else 'bot0_3_0'
+            tp = 0
+        else:
+            edge_id1 = 'bot2_1_0'
+            edge_id2 = 'bot1_3_0' if rn2 < 0.5 else 'left2_3_0'
+            tp = 1
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-X2':
+        idx = env.k.person.total
+        t =  env.time_counter / env.env_params.sims_per_step / env.env_params.horizon
+        rn = np.random.rand()
+        if t < 0.5:
+            edge_id1 = 'bot3_1_0'
+            edge_id2 = 'left1_3_0' if rn < 0.5 else 'bot0_3_0'
+            tp = 0
+        else:
+            edge_id1 = 'top3_3_0'
+            edge_id2 = 'left1_0_0' if rn < 0.5 else 'top0_1_0'
+            tp = 1
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-2':
+        # the request only appears at two different edges
+        idx = env.k.person.total
+        edge_list = env.edges.copy()
+        rn =  np.random.rand()
+        edge_id1 = 'bot3_1_0' if rn < 0.5 else 'top0_3_0'
+        edge_list.remove(edge_id1)
+        edge_id2 = 'top2_3_0' if rn < 0.5 else 'bot1_1_0'
+
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(20, env.inner_length - 20)
+    elif env.distribution == 'mode-3':
+        # the request appears at one edge before half of the time
+        # the request appears at another edge after half of the time
+        idx = env.k.person.total
+        edge_list = env.edges.copy()
+        time_ratio = env.time_counter / env.env_params.horizon
+        edge_id1 = 'bot3_1_0' if time_ratio < 0.5 else 'bot0_3_0'
+        edge_list.remove(edge_id1)
+        edge_id2 = 'top2_1_0' if time_ratio < 0.5 else 'top1_3_0'
+
+        per_id = 'per_' + str(idx)
+        pos = np.random.uniform(env.inner_length)
+    elif env.distribution == 'mode-4':
+        if env.time_counter % 20 != 1:
+            return
+        # the request appears at one edge before half of the time
+        # the request appears at another edge after half of the time
+        idx = env.k.person.total
+        edge_list = env.edges.copy()
+        time_ratio = env.time_counter / env.env_params.horizon
+        edge_id1 = 'bot3_1_0' if time_ratio < 0.5 else 'bot0_3_0'
+        edge_list.remove(edge_id1)
+        edge_id2 = 'top2_1_0' if time_ratio < 0.5 else 'top1_3_0'
+
+        per_id = 'per_' + str(idx)
+        # pos = np.random.uniform(env.inner_length)
+        pos = env.time_counter % env.grid_array['inner_length']
+    elif env.distribution == 'mode-5':
+        if env.time_counter % 5 != 1:
+            return
+        # the request appears at one edge before half of the time
+        # the request appears at another edge after half of the time
+        idx = env.k.person.total
+        edge_list = env.edges.copy()
+        time_ratio = env.time_counter / env.env_params.horizon
+        edge_id1 = 'bot3_1_0' if time_ratio < 0.5 else 'bot0_3_0'
+        edge_list.remove(edge_id1)
+        edge_id2 = 'top2_1_0' if time_ratio < 0.5 else 'top1_3_0'
+
+        per_id = 'per_' + str(idx)
+        # pos = np.random.uniform(env.inner_length)
+        pos = env.time_counter % env.grid_array['inner_length']
+    else:
+        raise NotImplementedError
+    return per_id, edge_id1, edge_id2, pos, tp
--- train_scripts/eval.sh
+++ train_scripts/eval.sh
@@ -1,6 +1,6 @@
-python train/eval_myppo.py test-stl-nw-profit-large-flow-more \
-    --experiment-name=test-stl-nw-profit-large-flow-more \
+python train/eval_myppo.py test-ltl-nw-noprofit \
+    --experiment-name=test-stl-nw-profit-X2-large \
     --algo=ppo \
     --num-processes=1 \
     --verbose \
-    --eval-ckpt=6
+    --eval-ckpt=0
