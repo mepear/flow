7875cd57696335964f49d44834883867841a5619
Shusheng Xu <xssstory@gmail.com>
2021-07-02 17:23:13 +0800

    training scripts of emissions

---
+++ train_scripts/test-stl-nw-noprofit-R-co2.sh
@@ -0,0 +1,24 @@
+python train/train.py \
+    test-stl-nw-noprofit-R-co2 \
+    --experiment-name=test-stl-nw-noprofit-R-co2 \
+    --num-env-steps=1600000000 \
+    --algo=ppo \
+    --use-gae \
+    --lr=2.5e-4 \
+    --clip-param=0.2 \
+    --value-loss-coef=0.5 \
+    --num-envs=800 \
+    --num-actors=8 \
+    --num-splits=2 \
+    --eval-num-processes=50 \
+    --num-steps=500 \
+    --num-mini-batch=4 \
+    --log-interval=1 \
+    --save-interval=32 \
+    --eval-interval=1000 \
+    --use-linear-lr-decay \
+    --popart-reward \
+    --entropy-coef=0.01 \
+    --gamma=0.999 \
+    --queue-size=5 \
+    --reuse=4
